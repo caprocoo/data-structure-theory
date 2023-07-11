9ba1fcb8d9dba8fd75113eacc4127fa965c049de
caprocoo <91231618+caprocoo@users.noreply.github.com>
2023-07-11 09:46:16 +0900

    init

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * packageName    : PACKAGE_NAME
+ * fileName       : Main
+ * author         : caprocoo
+ * date           : 2023-07-11
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-07-11        caprocoo       최초 생성
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
